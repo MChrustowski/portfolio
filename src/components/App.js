5d90bfd71bb8105eddf5671c26172bf973e5f127
Michał Chrustowski <chrustowski.m@gmail.com>
2018-10-06 01:34:57 +0200

    Initial webpack configuration

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+
+import reactIcon from "../assets/images/react-icon.png";
 
 const App = () => {
-  return <div>ReactReduxStarter</div>;
+  return (
+    <Container>
+      <img src={reactIcon} alt="React logo" />
+      <Header>React Redux Starter</Header>
+    </Container>
+  );
 };
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Header = styled.div`
+  color: red;
+  font-size: 30px;
+  text-align: center;
+`;
